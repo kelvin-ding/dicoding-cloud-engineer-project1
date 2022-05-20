07961ff31da95f39f85fcdd4bdce5b9c3474200c
Kelvin Ding <70010690+kelvin-ding@users.noreply.github.com>
2022-05-20 13:11:06 +0700

    revision

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+app.use(express.static(path.join(__dirname, "public")));
 
-var app = express();
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname + "/index.html"));
+  //__dirname : It will resolve to your project folder.
+});
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//add the router
+app.use("/", router);
+app.listen(process.env.port || 3000);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+console.log("Running at Port 3000");
